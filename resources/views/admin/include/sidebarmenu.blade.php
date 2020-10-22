aacef2c4edb0289fb243f80868d926ea0969155e
unknown <sukhwinder432111@gmail.com>
2020-10-22 12:02:47 +0530

    Redirect admin to distributor

--- resources/views/admin/include/sidebarmenu.blade.php
+++ resources/views/admin/include/sidebarmenu.blade.php
@@ -33,82 +33,9 @@
                 </ul>
             </li>
 
-            <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class="fa fa-laptop"></i>
-                    <span>Products</span>
-                </a>
-                <ul class="sub">
-                    <li><a  href="{{ route('distributor_coupon.distributor_coupons.index') }}">Distributor C</a></li>
-                    <li><a  href="">Create Products</a></li>
-                </ul>
-            </li>
+           
             
-            <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class="fa fa-laptop"></i>
-                    <span>Units</span>
-                </a>
-                <ul class="sub">
-                    <li><a  href="">All Units</a></li>
-                    <li><a  href="">Create Units</a></li>
-                </ul>
-            </li>
-
-            <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class="fa fa-laptop"></i>
-                    <span>Purchase</span>
-                </a>
-                <ul class="sub">
-                    <li><a  href="">All Purchase</a></li>
-                    <li><a  href="">Create Purchase</a></li>
-                </ul>
-            </li>
-
-            <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class="fa fa-laptop"></i>
-                    <span>Departments</span>
-                </a>
-                <ul class="sub">
-                    <li><a  href="">All Departments</a></li>
-                    <li><a  href="">Create Departments</a></li>
-                </ul>
-            </li>
-            
-           {{--  <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class="fa fa-laptop"></i>
-                    <span>Departments</span>
-                </a>
-                <ul class="sub">
-                    <li><a  href="">All Departments</a></li>
-                    <li><a  href="">Create Departments</a></li>
-                </ul>
-            </li> --}}
-            
-            <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class="fa fa-laptop"></i>
-                    <span>Issue</span>
-                </a>
-                <ul class="sub">
-                    <li><a  href="">All Issued Products</a></li>
-                    <li><a  href="">Issue Product</a></li>
-                </ul>
-            </li>
-
-            <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class="fa fa-laptop"></i>
-                    <span>Vendors</span>
-                </a>
-                <ul class="sub">
-                    <li><a  href="">All Vendors</a></li>
-                    <li><a  href="">Create Vendor</a></li>
-                </ul>
-            </li>
+           
             <li class="sub-menu">
                 <a href="javascript:;" >
                     <i class="fa fa-laptop"></i>
--- routes/admin.php
+++ routes/admin.php
@@ -17,7 +17,7 @@ Route::get('create','admin\AdminLoginController@create');
 
 Route::group(['middleware'=>'adminm'], function() {
 	// Route::view('/','admin.dashboard');
-    Route::get('/','admin\DashboardController@index')->name('dashboard');
+    Route::get('/','admin\DistributorController@index')->name('dashboard');
     Route::get('select_coupons','admin\CouponSelectionController@createSale')->name('coupon_selections.index');
     Route::match(['get','post'],'select_coupons/create','admin\CouponSelectionController@createSale')->name('coupon_selections.create');
     Route::post('coupon_transaction_save', 'admin\CouponSelectionController@coupon_transaction_save')->name('coupon_transaction_save');	
